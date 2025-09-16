09d372c5432ce4a901799f604cfbf8d8578d9330
patricio-moraga <62567586+patricio-moraga@users.noreply.github.com>
2025-09-16 12:20:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,9 @@ def predict():
 port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
